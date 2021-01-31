247079148afda2d1640b2e76423b6149125238c2
Amogh Dhavalikar <amoghdhavalikar@gmail.com>
2021-01-31 15:58:59 +0530

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -4,16 +4,16 @@ class Form {
   }
 
   display() {
-    var title = createElement('h2')
-    title.html("Welcome!!!");
-    title.position(160, 85);
+    // var title = createElement('h2')
+    // title.html("Welcome!!!");
+    // title.position(160, 85);
 
     var input = createInput("Name: ");
     var button = createButton('Continue with the above name');
 
 
-    input.position(140, 160);
-    button.position(130, 185);
+    input.position(displayWidth - 800, 80);
+    button.position(displayWidth - 800, 105);
 
     button.mousePressed(function () {
       input.hide();
@@ -26,7 +26,7 @@ class Form {
       player.update(name)
       player.updateCount(playerCount);
       var greeting = createElement('h3');
-      greeting.html("Hello, " + name + ". Thank you for visiting! Please click on 'Proceed' link, located at the very top")
+      greeting.html("Hello, " + name + ". Thank you for visiting! Please make a choice from any of the given options, located at the very top")
       greeting.position(70, 160)
       // var red = window.open(URL, ('entry/index.htm'))
 
